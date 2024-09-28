7fc5e648abc0d38631b2723c5497ff83b92f367d
Carlos Jesús Ordaz Hoyos <carlos.ordaz@acomo.com.pe>
2024-09-28 14:38:38 -0500

    xx

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // uri: 'http://localhost:4000',
-    uri: 'http://flyfar-gwdcgtc0adgtfbap.canadacentral-01.azurewebsites.net'
+    uri: 'https://flyfar-backend-abchh5bgh5eqfggs.eastus-01.azurewebsites.net/'
 }
